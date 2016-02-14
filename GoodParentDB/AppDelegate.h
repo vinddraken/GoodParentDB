da947497b9e71fe64472749c8d1d39e25627371a
vinddraken <vinddraken@gmail.com>
2016-02-14 19:55:44 +0100

    Update AppDelegate.h

--- GoodParentDB/AppDelegate.h
+++ GoodParentDB/AppDelegate.h
@@ -3,7 +3,7 @@
 //  GoodParentDB
 //
 //  Created by David Larsson on 2013-01-30.
-//  Copyright (c) 2013 David Larsson. All rights reserved.
+//  Copyright (c) 2013-2016 David Larsson. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
